10a14827e5e152f34f0274d02662ad524614893b
kaijieee <76198882+kaijieee@users.noreply.github.com>
2024-04-07 03:44:00 +0800

    updated app.py

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, request, redirect, render_template, session
-from sqlalchemy import create_engine, Column, String, Integer
+from sqlalchemy import create_engine, Column, String, Integer, Date, Float
 from sqlalchemy.orm import declarative_base, sessionmaker
 from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.utils import secure_filename
+import os
+from datetime import datetime
+from propertyForm import createProperty
 
 app = Flask(__name__)
 app.secret_key = "password"  # Change this to a random secret key
@@ -18,6 +22,31 @@ class SystemAdmin(Base):
     def __init__(self, username, password):
         self.username = username
         self.password = generate_password_hash(password)  # Hash the password
+
+class Property(Base):
+    __tablename__ = "Property"
+
+    ID = Column(Integer, primary_key=True)
+    propertyname = Column(String)
+    propertytype = Column(String)
+    district = Column(String)
+    bedroom_no = Column(Integer)
+    price = Column(Float)
+    psf = Column(Integer)
+    listing_date = Column(Date)
+    date_sold = Column(Date)
+    image_url = Column(String) 
+
+    def __init__(self, propertyname, propertytype, district, bedroom_no, price, psf, listing_date, date_sold, image_url):
+        self.propertyname = propertyname
+        self.propertytype = propertytype
+        self.district = district
+        self.bedroom_no = bedroom_no
+        self.price = price
+        self.psf = psf
+        self.listing_date = listing_date
+        self.date_sold = date_sold
+        self.image_url = image_url
 
 engine = create_engine("sqlite:///viewnest.db", echo=True)
 Base.metadata.create_all(bind=engine)
@@ -59,6 +88,43 @@ def dashboard():
 def logout():
     session.pop('user_id', None)
     return redirect('/')
+
+@app.route('/create_property', methods=['GET', 'POST'])
+def create_property():
+    CreateProperty = createProperty(request.form)
+    if request.method == 'POST':
+        propertyname = CreateProperty.propertyname.data
+        propertytype = CreateProperty.propertytype.data
+        district = CreateProperty.district.data
+        bedroom_no = CreateProperty.bedroom_no.data
+        price = CreateProperty.price.data
+        psf = CreateProperty.psf.data
+        image_file = request.files.get('image_url')
+        
+        if image_file:
+            filename = secure_filename(image_file.filename)
+            app.config['UPLOAD_FOLDER'] = './static/uploads/properties/'
+            image_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            image_file.save(image_path)
+        else:
+            image_path = None
+        
+        new_property = Property(propertyname=propertyname,
+                                propertytype=propertytype,
+                                district=district,
+                                bedroom_no=bedroom_no,
+                                price=price,
+                                psf=psf,
+                                listing_date=datetime.now().date(),
+                                date_sold=None,
+                                image_url=image_path)
+
+        db_session.add(new_property)
+        db_session.commit()
+
+        return redirect('/') # need to change to view_properties page
+    
+    return render_template('create_property.html', form=CreateProperty)
 
 if __name__ == '__main__':
     app.run(debug=True)
