7fbb29c05594be54aad101ae03f192e89682a4a4
areuim <60703978+areuim@users.noreply.github.com>
2024-05-10 01:06:43 +0800

    updated viewPropertyBoundary class

--- app/boundary/propertyBoundary.py
+++ app/boundary/propertyBoundary.py
@@ -1,19 +1,31 @@
-from flask import render_template
+from flask import render_template, request
 from app.control.propertyController import viewCountController, viewPropertyController
 from app.control import bp
 
+class viewPropertyBoundary:
+    def view_properties(self):
+        viewPropertyController.add_sample_properties()
+        properties = viewPropertyController.view_properties()
+        return render_template('property/view_properties.html', properties=properties)
+
+    def view_calculation(self):
+        return render_template('property/view_calculation.html')
+
+    def view_property_detail(self, property_id):
+        property = viewPropertyController.view_property_detail(property_id)
+        viewCountController.add_viewCount(property_id)
+        return render_template('property/view_property_detail.html', property=property)
+    
+property_boundary = viewPropertyBoundary()
+
 @bp.route('/view_properties')
 def view_properties():
-    viewPropertyController.add_sample_properties()
-    properties = viewPropertyController.view_properties()
-    return render_template('property/view_properties.html', properties=properties)
-
+    return property_boundary.view_properties()
+    
 @bp.route('/view_calculation')
 def view_calculation():
-    return render_template('property/view_calculation.html')
+    return property_boundary.view_calculation()
 
 @bp.route('/view_property_detail/<int:property_id>')
 def view_property_detail(property_id):
-    property = viewPropertyController.view_property_detail(property_id)
-    viewCountController.add_viewCount(property_id)
-    return render_template('property/view_property_detail.html', property=property)
+    return property_boundary.view_property_detail(property_id)
