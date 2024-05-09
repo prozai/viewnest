648a941c236afc0e8ab025e747fdeee993e25b20
kaijieee <76198882+kaijieee@users.noreply.github.com>
2024-05-10 00:44:34 +0800

    updated property boundary

--- app/boundary/propertyBoundary.py
+++ app/boundary/propertyBoundary.py
@@ -2,23 +2,56 @@ from flask import render_template, redirect, url_for
 from app.control.propertyController import createPropertyController, viewPropertiesController, updatePropertyController, deletePropertyController
 from app.control import bp
 
+class createPropertyBoundary:
+    def __init__(self):
+        self.controller = createPropertyController()
+        
+    def createProperty(self):
+        self.controller.REA_createProperty()
+        return render_template('REAgent/create_property.html')
+    
+class REAPropertiesBoundary:
+    def __init__(self):
+        self.controller = viewPropertiesController()
+        
+    def REAViewProperties(self):
+        properties = self.controller.REA_viewProperties()
+        return render_template('REAgent/REA_properties.html', properties=properties)
+
+class updatePropertyBoundary:
+    def __init__(self):
+        self.controller = updatePropertyController()
+        
+    def updateProperty(self, id):
+        property = self.controller.REA_updateProperty(id)
+        return render_template('REAgent/update_property.html', property=property)
+    
+class deletePropertyBoundary:
+    def __init__(self):
+        self.controller = deletePropertyController()
+        
+    def deleteProperty(self, id):
+        self.controller.REA_deleteProperty(id)
+        return redirect(url_for('route.REA_view_properties'))
+    
+create_property_boundary = createPropertyBoundary()
+REA_properties_boundary = REAPropertiesBoundary()
+update_property_boundary = updatePropertyBoundary()
+delete_property_boundary = deletePropertyBoundary()
+
 @bp.route('/create_property', methods=['GET', 'POST'])
 def create_property():
-    createPropertyController.REA_createProperty()
-    return render_template('REAgent/create_property.html')
+    return create_property_boundary.createProperty()
 
 @bp.route('/REA_properties')
-def view_REA_properties():
-    properties = viewPropertiesController.REA_viewProperties()
-    return render_template('REAgent/REA_properties.html', properties=properties)
+def REA_view_properties():
+    return REA_properties_boundary.REAViewProperties()
 
 @bp.route('/update_property/<int:id>/', methods=['GET', 'POST'])
 def update_property(id):
-    property = updatePropertyController.REA_updateProperty(id)
-    return render_template('REAgent/update_property.html', property=property)
+    return update_property_boundary.updateProperty(id)
 
 # Delete Property Listing
 @bp.route('/delete_property/<int:id>/', methods=['POST'])
 def delete_property(id):
-    deletePropertyController.REA_deleteProperty(id)
-    return redirect(url_for('route.view_REA_properties'))
+    return delete_property_boundary.deleteProperty(id)
