4a189b49754f38a96d6eb34ce65157541e7b3e57
kaijieee <76198882+kaijieee@users.noreply.github.com>
2024-05-12 22:01:50 +0800

    updated property boundary

--- app/boundary/propertyBoundary.py
+++ app/boundary/propertyBoundary.py
@@ -1,43 +1,58 @@
 from flask import render_template, redirect, url_for
-from app.control.propertyController import createPropertyController, viewPropertiesController, updatePropertyController, deletePropertyController
+from app.control.propertyController import *
 from app.control import bp
 
 class createPropertyBoundary:
-    def __init__(self):
-        self.controller = createPropertyController()
-        
     def createProperty(self):
-        self.controller.REA_createProperty()
+        createPropertyController.REA_createProperty()
         return render_template('REAgent/create_property.html')
     
 class REAPropertiesBoundary:
-    def __init__(self):
-        self.controller = viewPropertiesController()
-        
     def REAViewProperties(self):
-        properties = self.controller.REA_viewProperties()
+        properties = REAPropertiesController.REA_viewProperties()
         return render_template('REAgent/REA_properties.html', properties=properties)
 
 class updatePropertyBoundary:
-    def __init__(self):
-        self.controller = updatePropertyController()
-        
     def updateProperty(self, id):
-        property = self.controller.REA_updateProperty(id)
+        property = updatePropertyController.REA_updateProperty(id)
         return render_template('REAgent/update_property.html', property=property)
     
 class deletePropertyBoundary:
-    def __init__(self):
-        self.controller = deletePropertyController()
-        
     def deleteProperty(self, id):
-        self.controller.REA_deleteProperty(id)
+        deletePropertyController.REA_deleteProperty(id)
         return redirect(url_for('route.REA_view_properties'))
+
+class viewPropertyBoundary:
+    def view_properties(self):
+        viewPropertyController.add_sample_properties()
+        properties = viewPropertyController.view_properties()
+        return render_template('property/view_properties.html', properties=properties)
+
+    def view_calculation(self):
+        return render_template('property/view_calculation.html')
+
+    def view_property_detail(self, property_id):
+        property = viewPropertyController.view_property_detail(property_id)
+        viewCountController.add_viewCount(property_id)
+        return render_template('property/view_property_detail.html', property=property)
+
+class savePropertyBoundary:
+    def saveProperty(self):
+        savePropertyController.buyer_saveProperty()
+        return redirect(url_for('route.view_properties'))
+
+class sellerPropertiesBoundary:
+    def sellerViewProperties(self):
+        properties = sellerPropertiesController.seller_viewProperties()
+        return render_template('property/seller_properties.html', properties=properties)
     
 create_property_boundary = createPropertyBoundary()
 REA_properties_boundary = REAPropertiesBoundary()
 update_property_boundary = updatePropertyBoundary()
 delete_property_boundary = deletePropertyBoundary()
+property_boundary = viewPropertyBoundary()
+save_property_boundary = savePropertyBoundary()
+seller_properties_boundary = sellerPropertiesBoundary()
 
 @bp.route('/create_property', methods=['GET', 'POST'])
 def create_property():
@@ -54,3 +69,23 @@ def update_property(id):
 @bp.route('/delete_property/<int:id>/', methods=['POST'])
 def delete_property(id):
     return delete_property_boundary.deleteProperty(id)
+
+@bp.route('/view_properties')
+def view_properties():
+    return property_boundary.view_properties()
+    
+@bp.route('/view_calculation')
+def view_calculation():
+    return property_boundary.view_calculation()
+
+@bp.route('/view_property_detail/<int:property_id>')
+def view_property_detail(property_id):
+    return property_boundary.view_property_detail(property_id)
+
+@bp.route('/save_property', methods=['POST'])
+def save_property():
+    return save_property_boundary.saveProperty()
+
+@bp.route('/seller_properties')
+def seller_view_properties():
+    return seller_properties_boundary.sellerViewProperties()
