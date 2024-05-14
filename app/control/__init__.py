c4150c451029147fa980bdea16508997fc739f97
wzong003 <165866273+wzong003@users.noreply.github.com>
2024-05-14 18:14:42 +0800

    Delete app/control/__init__.py

--- app/control/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-
-adminBP = Blueprint('adminroutes', __name__)
-mainBP = Blueprint('routes', __name__)
-
-from app.control import adminController, mainController, loginController
