929c311f1e17009179e5b9176a74337bcf542b00
kaijieee <76198882+kaijieee@users.noreply.github.com>
2024-05-15 04:31:16 +0800

    updated property boundary

--- app/boundary/propertyBoundary.py
+++ app/boundary/propertyBoundary.py
@@ -2,26 +2,6 @@ from flask import render_template, redirect, url_for
 from app.control.propertyController import *
 from app.control import bp
 
-class createPropertyBoundary:
-    def createProperty(self):
-        createPropertyController.REA_createProperty()
-        return render_template('REAgent/create_property.html')
-    
-class REAPropertiesBoundary:
-    def REAViewProperties(self):
-        properties = REAPropertiesController.REA_viewProperties()
-        return render_template('REAgent/REA_properties.html', properties=properties)
-
-class updatePropertyBoundary:
-    def updateProperty(self, id):
-        property = updatePropertyController.REA_updateProperty(id)
-        return render_template('REAgent/update_property.html', property=property)
-    
-class deletePropertyBoundary:
-    def deleteProperty(self, id):
-        deletePropertyController.REA_deleteProperty(id)
-        return redirect(url_for('route.REA_view_properties'))
-
 class viewPropertyBoundary:
     def view_properties(self):
         viewPropertyController.add_sample_properties()
@@ -36,40 +16,8 @@ class viewPropertyBoundary:
         viewCountController.add_viewCount(property_id)
         return render_template('property/view_property_detail.html', property=property)
 
-class savePropertyBoundary:
-    def saveProperty(self):
-        savePropertyController.buyer_saveProperty()
-        return redirect(url_for('route.view_properties'))
-
-class sellerPropertiesBoundary:
-    def sellerViewProperties(self):
-        properties = sellerPropertiesController.seller_viewProperties()
-        return render_template('property/seller_properties.html', properties=properties)
-    
-create_property_boundary = createPropertyBoundary()
-REA_properties_boundary = REAPropertiesBoundary()
-update_property_boundary = updatePropertyBoundary()
-delete_property_boundary = deletePropertyBoundary()
 property_boundary = viewPropertyBoundary()
-save_property_boundary = savePropertyBoundary()
-seller_properties_boundary = sellerPropertiesBoundary()
-
-@bp.route('/create_property', methods=['GET', 'POST'])
-def create_property():
-    return create_property_boundary.createProperty()
-
-@bp.route('/REA_properties')
-def REA_view_properties():
-    return REA_properties_boundary.REAViewProperties()
-
-@bp.route('/update_property/<int:id>/', methods=['GET', 'POST'])
-def update_property(id):
-    return update_property_boundary.updateProperty(id)
-
-@bp.route('/delete_property/<int:id>/', methods=['POST'])
-def delete_property(id):
-    return delete_property_boundary.deleteProperty(id)
-
+    
 @bp.route('/view_properties')
 def view_properties():
     return property_boundary.view_properties()
@@ -82,10 +30,79 @@ def view_calculation():
 def view_property_detail(property_id):
     return property_boundary.view_property_detail(property_id)
 
-@bp.route('/save_property', methods=['POST'])
-def save_property():
-    return save_property_boundary.saveProperty()
 
-@bp.route('/seller_properties')
-def seller_view_properties():
-    return seller_properties_boundary.sellerViewProperties()
+class createPropertyPage():
+    @bp.route('/create_property', methods=['GET', 'POST'])
+    def create_property():
+        if request.method == 'POST':
+            try:
+                propertyname = request.form.get("propertyname")
+                propertytype = request.form.get("propertytype")
+                district=request.form.get("district")
+                bedroom_no=request.form.get("bedroom_no")
+                price=request.form.get("price")
+                psf=request.form.get("psf")
+                image_file=request.files.get('image_url')
+                selleremail=request.form.get("selleremail")
+
+                newProperty = createPropertyController()
+                property = newProperty.REA_createProperty(propertyname, propertytype, district, bedroom_no, price, psf, image_file, selleremail)
+
+                if property:
+                    return redirect(url_for('route.REA_view_properties'))
+                else:
+                    raise Exception('Error creating property')
+            except Exception as e:
+                print(e)
+        return render_template('REAgent/create_property.html')
+    
+class REAPropertiesPage:
+    @bp.route('/REA_properties')
+    def REA_view_properties():
+        properties = REAPropertiesController.REA_viewProperties()
+        return render_template('REAgent/REA_properties.html', properties=properties)
+    
+class updatePropertyPage:
+    @bp.route('/update_property/<int:id>/', methods=['GET', 'POST'])
+    def update_property(id):
+        if request.method == 'POST':
+            try:
+                propertyname = request.form['propertyname']
+                propertytype = request.form['propertytype']
+                district = request.form['district']
+                bedroom_no = request.form['bedroom_no']
+                price = request.form['price']
+                psf = request.form['psf']
+                selleremail = request.form['selleremail']
+                image_file = request.files.get('image_url')
+
+                updatedProperty = updatePropertyController.REA_updateProperty(id, propertyname, propertytype, district, bedroom_no, price, psf, selleremail, image_file)
+
+                if updatedProperty:
+                    flash("Updated successfully!")
+                else:
+                    flash("Property not found")
+            except Exception as e:
+                flash("Error updating property: " + str(e))
+
+        property = updatePropertyController.REA_getProperty(id)
+        return render_template('REAgent/update_property.html', property=property)
+
+    
+class deleteProperty:
+    @bp.route('/delete_property/<int:id>/', methods=['POST'])
+    def delete_property(id):
+        deletePropertyController.REA_deleteProperty(id)
+        return redirect(url_for('route.REA_view_properties'))
+
+class saveProperty:
+    @bp.route('/save_property', methods=['POST'])
+    def save_property():
+        savePropertyController.buyer_saveProperty()
+        return redirect(url_for('route.view_properties'))
+
+class sellerPropertiesPage:
+    @bp.route('/seller_properties')
+    def seller_view_properties():
+        properties = sellerPropertiesController.seller_viewProperties()
+        return render_template('property/seller_properties.html', properties=properties)
